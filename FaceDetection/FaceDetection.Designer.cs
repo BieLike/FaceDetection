1004d1e63b680eb7a9596a0fd2f57a2f7276ab1d
BieLike <b.vithawat@gmail.com>
2024-07-10 14:36:05 +0700

    Add project files.

---
+++ FaceDetection/FaceDetection.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace FaceDetection
+{
+    partial class FaceDetection
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.imgbWebcam = new Emgu.CV.UI.ImageBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.imgbWebcam)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(942, 156);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(124, 63);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Start detection and recognise";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // imgbWebcam
+            // 
+            this.imgbWebcam.Location = new System.Drawing.Point(12, 12);
+            this.imgbWebcam.Name = "imgbWebcam";
+            this.imgbWebcam.Size = new System.Drawing.Size(822, 648);
+            this.imgbWebcam.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imgbWebcam.TabIndex = 2;
+            this.imgbWebcam.TabStop = false;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(942, 346);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(124, 41);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Save face";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(913, 263);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(169, 30);
+            this.txtName.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(863, 266);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 24);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Name";
+            // 
+            // FaceDetection
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1117, 664);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.imgbWebcam);
+            this.Controls.Add(this.btnStart);
+            this.Font = new System.Drawing.Font("Saysettha OT", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "FaceDetection";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imgbWebcam)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnStart;
+        private Emgu.CV.UI.ImageBox imgbWebcam;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ FaceDetection/FaceDetection.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Face;
+using Emgu.CV.Structure;
+
+namespace FaceDetection
+{
+    public partial class FaceDetection : Form
+    {
+//list
+        CascadeClassifier faceDetected;
+        Image<Bgr, byte> frame;
+        VideoCapture cam;
+        Image<Gray, byte> result;
+        Image<Gray, byte> trainedFace = null;
+        Image<Gray, byte> grayFace = null;
+        List<Image<Gray, byte>> trainingImg = new List<Image<Gray, byte>>();
+        List<string> label = new List<string>();
+        List<string> user = new List<string>();
+        int count, numLabels, t;
+        string name, names = null;
+        private Dictionary<int, string> labelMap = new Dictionary<int, string>();
+
+        public FaceDetection()
+        {
+            InitializeComponent();
+//HaarCascade,haar.xml
+            string haarcascadePath = Path.Combine(Application.StartupPath, "haarcascade_frontalface_default.xml");
+            faceDetected = new CascadeClassifier(haarcascadePath);
+            //LoadTrainingData();
+
+            try
+            {
+                string labelsInfoPath = Path.Combine(Application.StartupPath, "Faces", "Faces.txt");
+                string Labelsinf = File.ReadAllText(labelsInfoPath);
+                string[] label2 = Labelsinf.Split(',');
+                numLabels = Convert.ToInt16(label2[0]);
+                count = numLabels;
+                string faceLoad;
+                for (int i=1; i<numLabels+1; i++)
+                {
+                    faceLoad = $"face{i}.bmp";
+                    string faceLoadPath = Path.Combine(Application.StartupPath, "Faces", faceLoad);
+                    trainingImg.Add(new Image<Gray, byte>(faceLoadPath));
+                    label.Add(label2[i]);
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Nothing in the database");
+            }
+        }
+
+        private void LoadTrainingData()
+        {
+            try
+            {
+                string labelsInfo = File.ReadAllText(Path.Combine(Application.StartupPath, "Faces", "Faces.txt"));
+                string[] labels = labelsInfo.Split(',');
+
+                count = Convert.ToInt32(labels[0]);
+                for (int i = 1; i <= count; i++)
+                {
+                    string faceFile = Path.Combine(Application.StartupPath, "Faces", $"face{i}.bmp");
+                    trainingImg.Add(new Image<Gray, byte>(faceFile));
+                    label.Add(labels[i]);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("No data in the database or error in loading training data: " + ex.Message);
+            }
+        }
+
+        private void imgbWebcam_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                count++;
+                Mat matFrame = cam.QueryFrame();
+                var frame = matFrame.ToImage<Bgr, byte>().Resize(320, 240, Inter.Cubic);
+                var grayFace = frame.Convert<Gray, byte>();
+                var detectedFaces = faceDetected.DetectMultiScale(grayFace, 1.2, 10, new Size(20, 20), Size.Empty);
+
+                Image<Gray, byte> trainedFace = null;
+                foreach (var f in detectedFaces)
+                {
+                    trainedFace = frame.Copy(f).Convert<Gray, byte>().Resize(100, 100, Inter.Cubic);
+                    break;
+                }
+
+                if (trainedFace != null)
+                {
+                    trainingImg.Add(trainedFace);
+                    label.Add(txtName.Text);
+
+                    string facesPath = Path.Combine(Application.StartupPath, "Faces");
+                    Directory.CreateDirectory(facesPath);
+                    File.WriteAllText(Path.Combine(facesPath, "Faces.txt"), trainingImg.Count.ToString() + ",");
+
+                    for (int i = 1; i <= trainingImg.Count; i++)
+                    {
+                        string faceFilePath = Path.Combine(facesPath, $"face{i}.bmp");
+                        trainingImg[i - 1].Save(faceFilePath);
+                        File.AppendAllText(Path.Combine(facesPath, "Faces.txt"), label[i - 1] + ",");
+                    }
+
+                    MessageBox.Show(txtName.Text + " Added Successfully");
+                }
+                else
+                {
+                    MessageBox.Show("No face detected. Please try again.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+    }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            cam = new VideoCapture();
+            cam.QueryFrame();
+//EventHandler
+            Application.Idle += new EventHandler(FrameProcedure);
+        }
+
+        private void FrameProcedure(object sender, EventArgs e)
+        {
+            user.Add("");
+//wtf all this
+            Mat matFrame = cam.QueryFrame();
+            frame = matFrame.ToImage<Bgr, byte>().Resize(320, 240, Inter.Cubic);
+            grayFace = frame.Convert<Gray, byte>();
+            var facesDetectedNow = faceDetected.DetectMultiScale(grayFace, 1.2, 10, new Size(20, 20), Size.Empty);
+            foreach (var f in facesDetectedNow)
+            {
+                result = frame.Copy(f).Convert<Gray, byte>().Resize(100, 100, Inter.Cubic);
+                frame.Draw(f, new Bgr(Color.Green), 2);
+                if (trainingImg.Count != 0)
+                {
+                    var termCriteria = new MCvTermCriteria(count, 0.001);
+                    var recognizer = new EigenFaceRecognizer(trainingImg.Count, double.PositiveInfinity);
+                    recognizer.Train(trainingImg.ToArray(), ConvertLabelsToInts(label).ToArray());
+                    var predictionResult = recognizer.Predict(result);
+                    var predictedLabel = predictionResult.Label;
+                    //name = labelMap.ContainsKey(predictedLabel) ? labelMap[predictedLabel] : "Unknown";
+                    if (labelMap.ContainsKey(predictedLabel))
+                    {
+                        name = labelMap[predictedLabel];
+                    }
+                    else
+                    {
+                        name = "Unknown";
+                    }
+                    //CvInvoke.PutText(frame, name, new Point(f.X - 2, f.Y - 2), FontFace.HersheyTriplex, 0.6, new MCvScalar(0, 0, 255), 1);
+
+                }
+                else
+                {
+                    name = "Unknown";
+                    //CvInvoke.PutText(frame, "Unknown", new Point(f.X - 2, f.Y - 2), FontFace.HersheyTriplex, 0.6, new MCvScalar(0, 0, 255), 1);
+                }
+                CvInvoke.PutText(frame, name, new Point(f.X - 2, f.Y - 2), FontFace.HersheyTriplex, 0.6, new MCvScalar(0, 0, 255), 1);
+                user.Add("");
+            }
+            imgbWebcam.Image = frame;
+            name = "";
+            user.Clear();
+        }
+        private List<int> ConvertLabelsToInts(List<string> labels)
+        {
+            var labelMapReverse = new Dictionary<string, int>();
+            var intLabels = new List<int>();
+
+            int currentLabel = 0;
+            foreach (var label in labels)
+            {
+                if (!labelMapReverse.ContainsKey(label))
+                {
+                    labelMapReverse[label] = currentLabel;
+                    labelMap[currentLabel] = label; // Map integer to name
+                    currentLabel++;
+                }
+                intLabels.Add(labelMapReverse[label]);
+            }
+
+            return intLabels;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
